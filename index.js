ee71cfe504dbc87d5e633ab55e0514279ed96f40
Pedro-Alonso831 <88863410+Pedro-Alonso831@users.noreply.github.com>
2021-08-16 22:18:44 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+require('dotenv').config();
+const app = require('./app');
+require('./database');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+async function main() {
+    await app.listen(app.get('port'));
+    console.log('Server on port', app.get('port'));
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+main();
